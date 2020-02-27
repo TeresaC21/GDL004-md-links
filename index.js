0fd1d6bf9cf65dc2dcb911d1073dab35bc336cad
Teresa Carbajal <Tere@MacBook-Air-de-Teresa.local>
2020-02-26 20:12:51 -0600

    test fetch status objs save array

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const path = require('path');
 const processExt = process.argv[2];
 const lista = [];
 const fetch = require('node-fetch')
-//const valid = require('./validate.js');
-//valid.hi();
+//const valid = require('./validate.js');//valid.hi();
 
 // se exporta
 function extractMD() {
@@ -20,7 +19,7 @@ function extractMD() {
 // *********** Function read file .md and search the links into *******
 function readLinks() {
     fs.readFile(processExt, 'utf8', (err, data) => {
-        console.log(processExt);
+        //console.log(processExt);
         if (err) {
             console.log(err);
         } else {
@@ -29,108 +28,65 @@ function readLinks() {
             let matchReg = data.match(expReg);
             //console.log(matchReg);
             if (matchReg) {
-                arrExpr(matchReg);
+                validateLinks(matchReg);
             } else {
                 console.log('Dont found');
-
             }
-            //let arrayMatch = matchReg.push(lista)
-            //console.log(arrayMatch, 'BUUUUUUU');
-            // return matchReg;
+            //let arrayMatch = lista.push(matchReg)//console.log(arrayMatch, 'BUUUUUUU');
         }
     })
 }
 
 // ya que itero en array de expresion regular, hacer push para pasar lista en el .map
-function arrExpr(matchReg) {
-    console.log(matchReg);
+// ******************** FETCH VALIDAR LINKS ************************
+function validateLinks(matchReg) {
+    //console.log(matchReg);
 
     for (let i = 0; i < matchReg.length; i++) {
-        console.log(matchReg[i]);
+        // console.log(matchReg[i]); //fetch('https://es.wikipedia.org/wiki/Markdown')
         fetch(matchReg[i])
             .then(res => {
-                //  console.log(res);
-                console.log(res.ok);
-                console.log(res.status)
-            });    
-    }
-    //console.log(res.url);
-    //console.log(res.ok);
-    //console.log(res.status);
-}
-//console.log(arrExpr(matchReg));
-
-// ******************** FETCH VALIDAR LINKS ************************
-function validarLinks() {
-    //fetch('https://es.wikipedia.org/wiki/Markdown')
-
-}
-
-
-/*   const obj = {
-                   url: res.url,
+                //console.log(res);//console.log(res.url)//console.log(res.ok);// console.log(res.status)// console.log(res.json());
+                const obj = {
+                    url: res.url,
                     ok: res.ok,
                     status: res.status
                 }
-                return obj */
+                //console.log(obj);
+                lista.push(obj);
+                console.log('es lista', lista);
 
-/*  fetch('https://es.wikipedia.org/wiki/Markdown')
-    .then(function(response) {
-        //console.log(response.status);
-        console.log(response.json());
-        
-      //return response.json();
-    })
-    .then(function(myJson) {
-      console.log(myJson);
-    });
- */
+                 if (res.status >= 200 && res.status < 300) {
+                    //console.log(matchReg[i].length, '20000000'); // contar links
+                    console.log(lista.length, '20000000'); // contar links
+                } else {
+                    console.log(lista.length, '40444444');
+                } 
+            })
+            .catch((error) =>
+                console.log('Hubo un problema con la petición Fetch:', error));
+    }
+}
+// libreria async
+// combinacion promises, promises.all, encadenamiento de promesas, async/await
+
+/* function counterStats(lista) {
+    let coounterOk = 0;
+    let counterFail = 0;
+for (let i = 0; i < lista.length; i++) {
+   fetch(lista[i])
+   .then((data) =>{
+    coounterOk++
+   })
+   .catch((data) =>{
+       counterFail++
+})
+    
+}
+
+} */
 
 module.exports.extractMD = extractMD;
-
-
-
-
-/*  matchReg.forEach(element => {
-                console.log(element);
-                let nuevo = lista.push(element); //
-                 console.log(nuevo);
-               // return nuevo
-            }) 
-//console.log(data);
-//let expReg = new RegExp("https?:\/\/[\w\-\.]+\.\w{2,5}\/?\S*");
-//let expReg = new RegExp("\\w+\\s","g");
-
-module.exports = {
-    extracting: function () {
-     
-        const path = require('path');
-        const processExt = process.argv[2];
-        const extPath = path.extname(processExt);
-        if (extPath == '.md') {
-            console.log(true, 'SOY .MD');
-            return readLinks();
-        } else {
-            console.log('NO SOY .MD');
-        }
-    },
-
-    // *********** Function read file .md and search the links into *******
-    readLinks: function () {
-        const fs = require('fs');
-        
-        const processExt = process.argv[2];
-     
-        fs.readFile(processExt, 'utf8', (err, data) => {
-            console.log(processExt);
-            if (err) {
-                console.log(err);
-            } else {
-                // let expReg = new RegExp(/(https?:\/\/[\w\.\-]+\.\w{2,5}(\/\S*)?)/g);
-                let expReg = new RegExp(/https?:\/\/[\w\.\-]+\.\w{2,5}[^\s\)]+/g)
-                let myArray = data.match(expReg);
-                console.log(myArray);
-            }
-        })
-    }
-} */
+/*module.exports = {
+    extracting: function () {},
+    readLinks: function () {} */
